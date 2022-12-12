9a8c5868a43d6b489c0c91c674410a55025630d4
WUILLANS <capo20033@gmail.com>
2022-12-11 21:04:25 -0500

    version completa railway

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -5,52 +5,66 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Estudiante;
 
-
 class PagesController extends Controller
 {
-    //////////////////// Portada //////////////////////////////////
-    public function fnIndex(){
+    ////PORTADA BIENVENIDA PARA EL PUBLICO GENERAL//////////////
+    public function fnIndex() {
         return view('welcome');
     }
 
-    //////////////////// CREATE ///////////////////////////////////
+    //////LECTURA////////////
+    public function fnLista() {
+        $xAlumnos = Estudiante::all();    //datos de db
+        return view('pagLista', compact('xAlumnos'));
+    }
+    public function fnEstActualizar($id){
+        $xActAlumnos = Estudiante::findOrFail($id);
+        return view('Estudiante.pagActualizar', compact('xActAlumnos'));
+    }
+    public function fnUpdate(Request $request,$id){ 
+
+        $xUpdateAlumnos = Estudiante::findOrFail($id);
+
+        $xUpdateAlumnos -> codEst = $request -> codEst;
+        $xUpdateAlumnos -> nomEst = $request -> nomEst;
+        $xUpdateAlumnos -> apeEst = $request -> apeEst;
+        $xUpdateAlumnos -> fnEst = $request -> fnEst;
+        $xUpdateAlumnos -> turMat = $request -> turMat;
+        $xUpdateAlumnos -> semMat = $request -> semMat;
+        $xUpdateAlumnos -> estMat = $request -> estMat;
+
+        $xUpdateAlumnos -> save();
+
+        return back()->with('msj','Se actualizo con exito...');
+    }
     public function fnRegistrar(Request $request){
 
-        //return $request->all();         //Prueba de "token" y datos recibidos
+        //return $request;         //Prueba de "token" y datos recibidos
 
         $request ->validate([
             'codEst' => 'required',
             'nomEst' => 'required',
             'apeEst' => 'required',
-            'fnaEst' => 'required',
+            'fnEst' => 'required',
             'turMat' => 'required',
             'semMat' => 'required',
             'estMat' => 'required'
         ]);
 
         $nuevoEstudiante = new Estudiante;
-        
         $nuevoEstudiante->codEst = $request->codEst;
         $nuevoEstudiante->nomEst = $request->nomEst;
         $nuevoEstudiante->apeEst = $request->apeEst;
-        $nuevoEstudiante->fnaEst = $request->fnaEst;
+        $nuevoEstudiante->fnEst = $request->fnEst;
         $nuevoEstudiante->turMat = $request->turMat;
         $nuevoEstudiante->semMat = $request->semMat;
         $nuevoEstudiante->estMat = $request->estMat;
         
         $nuevoEstudiante->save();
         
-        //$xAlumnos = Estudiante1::all();                      //Datos de BD
+        //$xAlumnos = Estudiante::all();                      //Datos de BD
         //return view('pagLista', compact('xAlumnos'));        //Pasar a pagLista
         return back()->with('msj','Se registro con éxito...'); //Regresar con msj
-    }
-
-    //////////////////// READ /////////////////////////////////////
-    public function fnLista(){
-
-        //$xAlumnos = Estudiante1::all();              //Todos los datos
-        $xAlumnos = Estudiante::paginate(4);
-        return view('dashboard', compact('xAlumnos'));
     }
 
     public function fnEstDetalle($id){
@@ -59,44 +73,19 @@ class PagesController extends Controller
         return view('Estudiante.pagDetalle', compact('xDetAlumnos'));
     }
 
-    //////////////////// UPDATE ///////////////////////////////////
-    public function fnEstActualizar($id){                   //Paso 1
-
-        $xActAlumnos = Estudiante::findOrFail($id);
-        return view('Estudiante.pagActualizar', compact('xActAlumnos'));
+    public function fnEliminar($id){
+        $deleteAlumno = Estudiante::findOrFail($id);
+        $deleteAlumno->delete();
+        return back()->with('msj','Seelimino con exito....');
+     }
+    
+    public function fnGaleria ($numero=69) {
+        //return "FOTO DE CODIGO: ".$numero ;
+        return view('pagGaleria', ['valor'=>$numero, 'otro'=>132]);
     }
+    
 
-    public function fnUpdate(Request $request, $id){        //Paso 2
 
-        //return $request->all();         //Prueba de "token" y datos recibidos
-
-        $xUpdateAlumnos = Estudiante::findOrFail($id);
-
-        $xUpdateAlumnos->codEst = $request->codEst;
-        $xUpdateAlumnos->nomEst = $request->nomEst;
-        $xUpdateAlumnos->apeEst = $request->apeEst;
-        $xUpdateAlumnos->fnaEst = $request->fnaEst;
-        $xUpdateAlumnos->turMat = $request->turMat;
-        $xUpdateAlumnos->semMat = $request->semMat;
-        $xUpdateAlumnos->estMat = $request->estMat;
-        
-        $xUpdateAlumnos->save();
-        
-        //$xAlumnos = Estudiante1::all();                        //Datos de BD
-        //return view('pagLista', compact('xAlumnos'));          //Pasar a pagLista
-        return back()->with('msj','Se actualizó con éxito...');  //Regresar con msj
-    }
-
-    //////////////////// DELETE /////////////////////////////////// 
-    public function fnEliminar(Request $request, $id){
+}    
 
     
-    }
-
-    //////////////////// EJEMPLO. RUTA CON VALIDACIÓN /////////////
-    public function fnGaleria($numero=0){
-        $valor = $numero;
-        $otro  = 25;
-        return view('pagGaleria', compact('valor','otro'));
-    }
-}
--- resources/views/pagLista.blade.php
+++ resources/views/pagLista.blade.php
@@ -8,12 +8,11 @@
     @if(session('msj'))
         <div class="alert alert-success">
             {{ session('msj') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="close"></button>
         </div>
     @endif
-
-    <div class="btn btn-success d-grid fs-5 mb-2">Registrar nuevo seguimiento...</div>
-
-    <form action="" method="post" class="d-grid gap-2">
+    
+    <form action="{{ route('Estudiante.xRegistrar') }}" method="post" class="d-grid gap-2">
         @csrf
 
         @error('codEst')
@@ -22,23 +21,24 @@
             </div>
         @enderror
 
-        @error($errors)
+        @error('nomEst')
             <div class="alert alert-danger">
                 El nombre es requerido 
             </div>
         @enderror
 
-        @if(true)
+        @if($errors -> has('apeEst'))
             <div class="alert alert-warning alert-dismissible fade show" role="alert">
                 El <strong>apellido</strong> es requerido 
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
         @endif
+    
 
         <input type="text" name="codEst" placeholder="Código" value="{{ old('codEst')}}" class="form-control mb-1">
         <input type="text" name="nomEst" placeholder="Nombres" value="{{ old('nomEst')}}" class="form-control mb-1">
         <input type="text" name="apeEst" placeholder="Apellidos" value="{{ old('apeEst')}}" class="form-control mb-1">
-        <input type="text" name="fnaEst" placeholder="Fecha de nacimiento" value="{{ old('fnaEst')}}" class="form-control mb-1">
+        <input type="text" name="fnEst" placeholder="Fecha de nacimiento" value="{{ old('fnEst')}}" class="form-control mb-1">
         <select name="turMat" class="form-control mb-1">
             <option value="">Seleccione...</option>
             <option value="1">Turno Día</option>
@@ -59,7 +59,6 @@
         <button class="btn btn-success" type="submit">Agregar</button>
     </form>
 
-    <h3>Lista. Estamos en pag. {{ $xAlumnos->currentPage() }} de {{ $xAlumnos->count() }}</h3> 
    
     <div class="btn btn-dark d-grid fs-5 mb-2 bt-2">Lista de seguimiento...</div>
     
@@ -84,7 +83,7 @@
                     </a>
                 </td>
                 <td>
-                    <form action="" method="post" class="d-inline">
+                    <form action="{{ route('Estudiante.xEliminar', $item->id) }}" method="post" class="d-inline">
                         @method('DELETE')
                         @csrf
                         <button type="submit" class="btn btn-danger btn-sm">
@@ -92,13 +91,14 @@
                         </button>
                     </form>
                     ...
-                    <a class="btn btn-warning btn-sm" href="">
+                    <a class="btn btn-warning btn-sm" href="{{ route('Estudiante.xActualizar', $item->id) }}">
                     A
                     </a>
                 </td>
             </tr>
             @endforeach
         </tbody>
+
 
         <thead class="table-secondary">
             <tr>
@@ -108,8 +108,7 @@
 
     </table>
 
-    {{ $xAlumnos->links() }}
+
    
+
 @endsection
-
-
